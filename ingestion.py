5d2f5742ec20059fdce99a37eeb2f6f24ac0d24d
Sagar Jain <geekysagarjain@gmail.com>
2025-07-16 14:01:05 +0530

    added the some comments in the code

--- ingestion.py
+++ ingestion.py
@@ -11,22 +11,22 @@ if __name__=="__main__":
 
     print("\n Starting the Ingetion...\n")
 
-    loader=TextLoader(r"C:\Users\user\Documents\ai\langchain_vector_dbs\medium_blog_1.txt", encoding='utf-8')
+    loader=TextLoader(r"C:\Users\user\Documents\ai\langchain_vector_dbs\medium_blog_1.txt", encoding='utf-8') #Load Data (Document Loader)
     document=loader.load()
     
     print("spliting...\n")
 
-    text_splitter=CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter=CharacterTextSplitter(chunk_size=1000, chunk_overlap=0) #Split Text (with 100 tokens)
 
-    texts=text_splitter.split_documents(document)
+    texts=text_splitter.split_documents(document) #Split Text (Text Splitter)
 
     print(f"created {len (texts)} chunks\n")
 
-    embeddings=OpenAIEmbeddings(openai_api_type=os.environ.get("OPEN_API_KEY"))
+    embeddings=OpenAIEmbeddings(openai_api_type=os.environ.get("OPEN_API_KEY")) #(Embedding Model) Generate Embeddings
 
     print("Ingeting in Pinecone\n")
 
-    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ['INDEX_NAME'])
-    
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ['INDEX_NAME']) #Store Vectors (Vector Store)
+
     print("finish")
     
